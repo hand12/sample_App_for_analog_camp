12238ee28dadf9b6a383d2d21e2600173590880c
Yusuke Yamashita <yusuke.yamashita@livesense.co.jp>
2018-01-18 21:30:45 +0900

    投稿・一覧表示まで

--- app/controllers/surprise_thanks_controller.rb
+++ app/controllers/surprise_thanks_controller.rb
@@ -1,5 +1,18 @@
 class SurpriseThanksController < ApplicationController
   def index
+    @surprise_thanks = SurpriseThank.all
+  end
 
+  def new
+  end
+
+  def create
+    SurpriseThank.create(thank_params)
+  end
+
+  private
+
+  def thank_params
+    params.require(:surprise_thank).permit(:title, :image, :episode, :dear_name)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'top', to: 'tops#index'
   get 'surprise_thanks', to: 'surprise_thanks#index'
+  get 'surprise_thanks/new', to: 'surprise_thanks#new'
+  post 'surprise_thanks', to: 'surprise_thanks#create'
 end
